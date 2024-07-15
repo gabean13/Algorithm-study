8c4aa801949a95a5d870f2e7b7973799f738c0ec
Gabin <gazette9@naver.com>
2024-07-16 00:55:13 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1552 ms, Memory: 338668 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -7,16 +7,17 @@ class Main {
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        int[] arr = new int[10001];
 
         for(int i = 0; i < N; i++){
-            arr[i] = Integer.parseInt(br.readLine());
+            arr[Integer.parseInt(br.readLine())]++;
         }
 
-        Arrays.sort(arr);
-
-        for(int i = 0; i < N; i++){
-		    sb.append(arr[i]).append("\n");
+        for(int i = 1; i < 10001; i++){
+            while(arr[i] > 0){
+		        sb.append(i).append("\n");
+                arr[i]--;
+            }
         }
 
         System.out.println(sb);
