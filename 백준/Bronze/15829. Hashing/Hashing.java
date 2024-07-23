1e8c51efd4439838c21d231a8c95288c2f116f49
Gabin <gazette9@naver.com>
2024-07-24 01:25:23 +0900

    [Bronze II] Title: Hashing, Time: 132 ms, Memory: 14268 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -5,17 +5,19 @@ class Main {
 	static public void main(String []args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int M = 1234567891;
+        
 
         String str = br.readLine();
         char[] charArr = str.toCharArray();
+        int M = 1234567891;
 
-        int hash = 0;
+        long hash = 0;
+        long pow = 1;
         for(int i = 0; i < charArr.length; i++){
-            int a = (int)charArr[i] - 96;
-            hash = (hash + a * (int)Math.pow(31, i)) % M;
+            hash += (((int)charArr[i] - 96) * pow) % M;
+            pow = (pow * 31) % M;
         }
 
-        System.out.println(hash);
+        System.out.println(hash % M);
     }
 }
