cf84905b937cc1d3fb70ea3dc27889c4984221a8
Gabin <gazette9@naver.com>
2024-08-05 16:45:50 +0900

    [Silver III] Title: 두 수의 합, Time: 252 ms, Memory: 28240 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	static public void main(String []args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int answer = 0;
+        int[] arr = new int[1000001];
+
+        //입력 처리
+        int n = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            arr[Integer.parseInt(st.nextToken())] = 1;
+        }
+
+        int x = Integer.parseInt(br.readLine());
+
+        //구현
+        for(int i = 1; i < ((x > 1000001) ? 1000001 : x); i++){
+            if(arr[i] == 1 && (x-i) <= 1000000 && arr[x - i] == 1){
+                answer++;
+            }
+        }
+
+        System.out.println(answer/2);
+    }
+}
