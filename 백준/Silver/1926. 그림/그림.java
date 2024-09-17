00731da4df5b89d755ecaa3eac2296700e48a0c7
Gabin <choegabin171@gmail.com>
2024-09-17 15:19:08 +0900

    [Silver I] Title: 그림, Time: 384 ms, Memory: 43232 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -2,17 +2,21 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+    static int[][] matrix;
+    static boolean[][] visited;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int n, m;
 	static public void main(String []args) throws IOException {
         //1. 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        //세로 가로 입력
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        int[][] matrix = new int[n][m];
-        boolean[][] visited = new boolean[n][m];
+        matrix = new int[n][m];
+        visited = new boolean[n][m];
         for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < m; j++){
@@ -21,45 +25,47 @@ class Main {
         }
 
         //2. 계산
-        int[] dx = {0, 1, 0, -1};
-        int[] dy = {1, 0, -1, 0};
-        Queue<Node> queue = new LinkedList<>();
-
         int pictureCnt = 0;
-        int max = 0;
+        int maxSize = 0;
+
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 int pictureSize = 0;
                 //방문하지 않은 그림의 시작점인지 검사
-                if(matrix[i][j] == 1 && visited[i][j] == false) {
-                    queue.add(new Node(i, j));
-                    visited[i][j] = true;
+                if(matrix[i][j] == 1 && !visited[i][j]) {
+                    int currentSize = bfs(i, j);
                     pictureCnt++;
-                    pictureSize++;
-                }
-                //그림 검사 시작
-                while(!queue.isEmpty()) {
-                    Node node = queue.poll();
-                    for(int dir = 0; dir < 4; dir++){
-                        int nx = node.x + dx[dir];
-                        int ny = node.y + dy[dir];
-                        if(nx >= 0 && nx < n && ny >= 0 && ny < m){
-                            if(matrix[nx][ny] == 1 && visited[nx][ny] == false) {
-                                queue.add(new Node(nx, ny));
-                                visited[nx][ny] = true;
-                                pictureSize++;
-                            }
-                        }
-                    }
-                }
-
-                if(pictureSize > max) {
-                    max = pictureSize;
+                    maxSize = Math.max(maxSize, currentSize);
                 }
             }
         }
+
         System.out.println(pictureCnt);
-        System.out.println(max);
+        System.out.println(maxSize);
+    }
+
+    private static int bfs(int startX, int startY) {
+        int pictureSize = 1;
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(new Node(startX, startY));
+        visited[startX][startY] = true;
+        while(!queue.isEmpty()) {
+            Node node = queue.poll();
+            for(int dir = 0; dir < 4; dir++){
+                int nx = node.x + dx[dir];
+                int ny = node.y + dy[dir];
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m){ // 범위 체크
+                    if(matrix[nx][ny] == 1 && visited[nx][ny] == false) {
+                        queue.add(new Node(nx, ny));
+                        visited[nx][ny] = true;
+                        pictureSize++;
+                    }
+                }
+            }
+        }
+
+        return pictureSize;
     }
 }
 
