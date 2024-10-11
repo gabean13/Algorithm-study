35632098e0f183f12f29d5ce2baa4380eaaf1a11
Gabin <choegabin171@gmail.com>
2024-10-11 22:44:22 +0900

    [level 1] Title: 숫자 짝꿍, Time: 126.37 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -3,37 +3,34 @@ import java.util.*;
 class Solution {
     public String solution(String X, String Y) {
         
-        char[] xArr = X.toCharArray();
-        Map<Integer, Integer> xMap = new TreeMap<>();
-        for(char ch : xArr) {
+        int[] xArr = new int[10];
+        int[] yArr = new int[10];
+        char[] x = X.toCharArray();
+        char[] y = Y.toCharArray();
+        
+        for(char ch : x) {
             int num = (int)(ch-'0');
-            xMap.put(num, xMap.getOrDefault(num, 1) + 1);
+            xArr[num] += 1;
         }
         
-        char[] yArr = Y.toCharArray();
-        List<Integer> list = new ArrayList<>();
-        for(char ch : yArr) {
+        for(char ch : y) {
             int num = (int)(ch-'0');
-            if(xMap.containsKey(num) && xMap.get(num) > 1) {
-                xMap.put(num, xMap.get(num)-1);
-                list.add(num);
-            }
+            yArr[num] += 1;
         }
         
-        list.sort(Collections.reverseOrder());
         StringBuilder sb = new StringBuilder();
-        
-        for(int num : list) {
-            sb.append(num);    
+        for(int i = 9; i >= 0; i--) {
+            if(xArr[i] == 0 || yArr[i] == 0) continue;
+            int repeat = xArr[i] > yArr[i] ? yArr[i] : xArr[i];
+            sb.append(Integer.toString(i).repeat(repeat));
         }
         
         String answer = sb.toString();
-        if(sb.length() == 0) {
+        if(answer.length() == 0) {
             return "-1";
         } else if(answer.replace("0", "").length() == 0) {
             return "0";
         }
-        
-        return sb.toString();
+        return answer;
     }
 }
