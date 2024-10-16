2d3d61d37efc8c05bca7a76924ec1253b480caf1
Gabin <choegabin171@gmail.com>
2024-10-16 15:13:18 +0900

    [Silver I] Title: IOIOI, Time: 228 ms, Memory: 22044 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,52 +1,34 @@
 import java.io.*;
+
 import java.util.*;
+
 public class Main {
-	static int SLength;
-	static String[] sArr;
-	static int N;
-	static String Pn;
-	static int PnLength;
+
 	public static void main(String...args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		SLength = Integer.parseInt(br.readLine());
-		sArr = br.readLine().split("");
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append("I");
-		for(int i = 0; i < N; i++) {
-			sb.append("OI");
-		}
-		
-		Pn = sb.toString();
-		PnLength = Pn.length();
+		int n = Integer.parseInt(br.readLine());
+		int sLength = Integer.parseInt(br.readLine());
+		char[] sArr = br.readLine().toCharArray();
+
 	
 		int answer = 0;
-		for(int i = 0; i <= SLength - PnLength; i++) {
-			if(sArr[i].equals("I")) {
-				answer += dp(i, 1);
-			}	
-		}
-
-		System.out.println(answer);		
-	}
-	
-	public static int dp(int index, int stackSize) {
-		if(stackSize == PnLength) {
-			if(sArr[index].equals("I")) {
-				return 1;
-			} else {				
-				return 0;
+		for(int i = 0; i < sArr.length - 2; i++) {
+			int ioiCnt = 0;
+			if(sArr[i] == 'O') {
+				continue;
+			} else {
+				while(i + 2 < sArr.length && sArr[i + 1] == 'O' && sArr[i + 2] == 'I') {
+					ioiCnt++;
+					if(ioiCnt == n) {
+						ioiCnt--;
+						answer++;
+					}
+					i+=2;
+				}
+				ioiCnt = 0;
 			}
 		}
 
-		if(index >= SLength - 1) return 0;
-		if (sArr[index + 1].equals("O") && stackSize % 2 == 1) { //I다음 O일때
-			return dp(index + 1, stackSize + 1);
-		} else if(sArr[index + 1].equals("I") && stackSize % 2 == 0) { //O다음 I일
-			return dp(index + 1, stackSize + 1);
-		}
-		
-		return 0;
+		System.out.println(answer);		
 	}
 }
