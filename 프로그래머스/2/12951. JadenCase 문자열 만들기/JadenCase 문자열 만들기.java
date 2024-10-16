a73dc779fd0a2dc2cc25629d6d403b48b901269b
Gabin <choegabin171@gmail.com>
2024-10-16 10:55:42 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.65 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,28 +1,17 @@
 import java.util.*;
 
 class Solution {
-    public String solution(String s) {
-   
-        //공백, 숫자, 문자
+    public String solution(String s) {  
+        //공백, 숫자, 문자  
+        String[] arr = s.toLowerCase().split("");
         StringBuilder sb = new StringBuilder();
-        char[] arr = s.toCharArray();
         
-        //첫번째 문자 대문자 처리
-        if(arr[0] >= 'a' && arr[0] <= 'z') {
-            arr[0] = Character.toUpperCase(arr[0]);
+        boolean flag = true;
+        
+        for(String str : arr) {
+            sb.append(flag ? str.toUpperCase() : str);
+            flag = str.equals(" ") ? true : false;
         }
-        sb.append(arr[0]);
-        
-        for(int i = 1; i < arr.length; i++) {
-            if(arr[i-1] == ' ' && arr[i] >= 'a' && arr[0] <= 'z') {
-                arr[i] = Character.toUpperCase(arr[i]);
-            }
-            else if (arr[i-1] != ' ' && arr[i] >= 'A' && arr[0] <= 'Z') {
-                arr[i] = Character.toLowerCase(arr[i]);
-            }
-            sb.append(arr[i]);
-        }
-        
          
         return sb.toString();
     }
