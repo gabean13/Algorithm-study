a8993489e1c3223d0bbcf443cf3357c610c1d52b
Gabin <choegabin171@gmail.com>
2024-10-18 13:56:47 +0900

    [Silver IV] Title: solved.ac, Time: 240 ms, Memory: 32396 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -11,22 +11,45 @@ public class Main {
 		int removeCnt = (int)(n * 0.15 + 0.5);
 		
 		//의견 입력
-		int[] arr = new int[n];
+		int[] arr = new int[31];
 		for(int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+			int index = Integer.parseInt(br.readLine());
+			arr[index] += 1;
 		}
 		
-		//의견 정렬
-		Arrays.sort(arr);
+		//제외되는 사람 앞 부분 제거
+		int idx = 1;
+		int removed = 0;
+		while(removed < removeCnt) {
+			if (arr[idx] == 0) {
+				idx++;
+				continue;
+			} 
+			arr[idx]--;
+			removed++;
+		}
 		
-		//의견 계산
+		//제외되는 사람  부분 제거
+		idx = 30;
+		removed = 0;
+		while(removed < removeCnt) {
+			if (arr[idx] == 0) {
+				idx--;
+				continue;
+			} 
+			arr[idx]--;
+			removed++;
+		}
+		
+		//의견 합 계산
 		double difficultySum = 0;
-		for(int i = removeCnt; i < n - removeCnt; i++) {
-			difficultySum += arr[i];
+		for(int i = 1; i < 31; i++) {
+			difficultySum += (arr[i] * i);
 		}
+		
+		//의견 결과 계산
 		int answer = (int)(difficultySum / (n - removeCnt*2) + 0.5);
 		
 		System.out.println(answer);
-		
 	}
 }
