a84bcce0f5ee39422079585f4b40ad02a4f52349
Gabin <choegabin171@gmail.com>
2024-10-21 13:44:42 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 108 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -9,7 +9,6 @@ public class Main {
 		
 		sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
 		recurv(0, round, "");
-		sb.append("라고 답변하였지.").append("\n");
 		System.out.println(sb.toString());
 	}
 	
@@ -17,6 +16,7 @@ public class Main {
 		if(cr == round) {
 			sb.append(prefix).append("\"재귀함수가 뭔가요?\"").append("\n");
 			sb.append(prefix).append("\"재귀함수는 자기 자신을 호출하는 함수라네\"").append("\n");
+			sb.append(prefix).append("라고 답변하였지.").append("\n");
 			return;
 		}
 		sb.append(prefix).append("\"재귀함수가 뭔가요?\"").append("\n");
@@ -24,7 +24,7 @@ public class Main {
 		sb.append(prefix).append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.").append("\n");
 		sb.append(prefix).append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"").append("\n");
 		
-		recurv(cr+1, round, prefix += "____");
+		recurv(cr+1, round, prefix + "____");
 		
 		sb.append(prefix).append("라고 답변하였지.").append("\n");
 	}
