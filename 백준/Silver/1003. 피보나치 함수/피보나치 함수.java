943a834531ac136e890e5888fe029ecf484da95e
Gabin <choegabin171@gmail.com>
2024-10-31 14:49:35 +0900

    [Silver III] Title: 피보나치 함수, Time: 124 ms, Memory: 16004 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -2,19 +2,19 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static long[][] arr = new long[41][2];
+	static long[][] fibonacci = new long[41][2];
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		arr[0][0] = 1;
-		arr[0][1] = 0;
+		fibonacci[0][0] = 1;
+		fibonacci[0][1] = 0;
 		
-		arr[1][0] = 0;
-		arr[1][1] = 1;
+		fibonacci[1][0] = 0;
+		fibonacci[1][1] = 1;
 		
-		int round = Integer.parseInt(br.readLine());
+		int testCases = Integer.parseInt(br.readLine());
 		
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < round; i++) {
+		for(int i = 0; i < testCases; i++) {
 			int num = Integer.parseInt(br.readLine());
 			long[] answer = fibo(num);
 			sb.append(answer[0] + " " + answer[1]).append("\n");
@@ -24,19 +24,17 @@ public class Main {
 	}
 	
 	static long[] fibo (int n) {
-		if(n == 1) {
-			return arr[1];
+		if(n == 1 || n == 0) {
+			return fibonacci[n];
 		} 
 		
-		if(n == 0) {
-			return arr[0];
+		if(fibonacci[n][0] != 0 || fibonacci[n][1] != 0) {
+			return fibonacci[n];
 		}
-
- 		if(arr[n][0] == 0 && arr[n][1] == 0) {
-			arr[n][0] = fibo(n-1)[0] + fibo(n-2)[0]; 
-			arr[n][1] = fibo(n-1)[1] + fibo(n-2)[1];
-		}
- 		
- 		return arr[n];
+		
+		fibonacci[n][0] = fibo(n-1)[0] + fibo(n-2)[0]; 
+		fibonacci[n][1] = fibo(n-1)[1] + fibo(n-2)[1];
+		
+ 		return fibonacci[n];
 	}
 }
