d455ceff68199eefbfb6ba64fdf67fea3f31541b
Gabin <choegabin171@gmail.com>
2024-11-13 09:41:53 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str1 = br.readLine();
+		String str2 = br.readLine();
+		int[] str1Arr = new int[26];
+		int[] str2Arr = new int[26];
+		for(char ch : str1.toCharArray()) {
+			str1Arr[ch - 'a'] += 1;
+		}
+		
+		int answer = 0;
+		for(char ch : str2.toCharArray()) {
+			if(str1Arr[ch - 'a'] > 0) {
+				str1Arr[ch - 'a']--;
+				answer++;
+			}
+		}
+		
+		System.out.println(str1.length() + str2.length() -  answer*2);
+	}
+}
