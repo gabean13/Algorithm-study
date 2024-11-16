ff1f8a251494c449bf480962cf28f294987fccaa
Gabin <choegabin171@gmail.com>
2024-11-16 22:37:14 +0900

    [Gold III] Title: 불!, Time: 516 ms, Memory: 82404 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -2,96 +2,90 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int R = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-
-        char [][] matrix = new char[R][C];
-        int[][] fireDist = new int[R][C];
-        int[][] jihoonDist = new int[R][C];
-        
-        Queue<Node> queue = new LinkedList<>();
-        int jx = 0, jy = 0;
-        for(int i = 0; i < R; i++) {
-            char[] input = br.readLine().toCharArray();
-            for(int j = 0; j < C; j++) {
-                matrix[i][j] = input[j];
-                fireDist[i][j] = -1;
-                jihoonDist[i][j] = -1;
-                if(input[j] == 'F') {
-                	queue.add(new Node(i, j));
-                	fireDist[i][j] = 1;
-                }
-                if(input[j] == 'J') {
-                    jx = i;
-                    jy = j;
-                }
-            }
-        }
-        
-        //1. 불 이동 경로를 dist로 표시
-        int[] dx = {1, 0, -1, 0};
-        int[] dy = {0, 1, 0, -1};
-     
-        while(!queue.isEmpty()) {
-        	Node node = queue.poll();
-        	int cx = node.x;
-        	int cy = node.y;
-        	for(int i = 0; i < 4; i++) {
-        		int nx = cx + dx[i];
-        		int ny = cy + dy[i];
-        		if(nx < 0 || ny < 0 || nx >= R || ny >= C) {
-        			continue;
-        		}
-        		if(fireDist[nx][ny] == -1 && matrix[nx][ny] != '#') {
-        			queue.add(new Node(nx, ny));
-        			fireDist[nx][ny] = fireDist[cx][cy] + 1;
-        		}
-        	}
-        }
-        
-
-        //2. 지훈이 이동경로
-        queue.add(new Node(jx, jy));
-        jihoonDist[jx][jy] = 1;
-        
-        int minDist = 1000001;
-        while(!queue.isEmpty()) {
-        	Node node = queue.poll();
-        	int cx = node.x;
-        	int cy = node.y;
-        	if(cx == 0 || cy == 0 || cx == R-1 || cy == C-1) {
-				if(jihoonDist[cx][cy] < minDist) {
-					minDist = jihoonDist[cx][cy];
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int height = Integer.parseInt(st.nextToken());
+		int width = Integer.parseInt(st.nextToken());
+		
+		char[][] matrix = new char[height][width];
+		
+		int[][] fDist = new int[height][width];
+		int[][] jDist = new int[height][width];
+		Queue<Node> fq = new LinkedList<>();
+		Queue<Node> jq = new LinkedList<>();
+		for(int y = 0; y < height; y++) {
+			char[] input = br.readLine().toCharArray();
+			for(int x = 0; x < width; x++) {
+				matrix[y][x] = input[x];
+				if(matrix[y][x] == 'J') {
+					jq.add(new Node(x, y));
+					jDist[y][x] = 1;
+					if(x == 0 || y == 0 || x == width-1 || y == height-1) {
+		                System.out.println(1);
+		                return;
+		            }
+				}
+				if(matrix[y][x] == 'F') {
+					fq.add(new Node(x, y));
+					fDist[y][x] = 1;
 				}
 			}
-        	for(int i = 0; i < 4; i++) {
-        		int nx = cx + dx[i];
-        		int ny = cy + dy[i];
-        		if(nx < 0 || ny < 0 || nx >= R || ny >= C) {
-        			continue;
-        		}
-        		if(jihoonDist[nx][ny] == -1 && matrix[nx][ny] != '#' 
-        				&& (fireDist[nx][ny] > jihoonDist[cx][cy] + 1 || fireDist[nx][ny] == -1)) {
-        			queue.add(new Node(nx, ny));
-        			jihoonDist[nx][ny] = jihoonDist[cx][cy] + 1;
-        		}
-        	}
-        }
-        
-        System.out.println((minDist == 1000001) ? "IMPOSSIBLE" : minDist);
-    }
-}
-
-class Node {
-    int x;
-    int y;
-
-    public Node(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+		}
+		
+		int[] dx = {1, -1, 0, 0};
+		int[] dy = {0, 0, 1, -1};
+		
+		//불이 난 시간을 fireDist에 저장
+		//지훈이는 불이 난 시간이랑 비교해서 그거보다 작아야지만 이동가능
+		
+		//1. 불 이동을 기록한다
+		while(!fq.isEmpty()) {
+			Node node = fq.poll();
+			int cx = node.x;
+			int cy = node.y;
+			for(int i = 0; i < 4; i++) {
+				int nx = cx + dx[i];
+				int ny = cy + dy[i];
+				if(nx < 0 || ny < 0 || nx >= width || ny >= height) continue;
+				if(matrix[ny][nx] != '#' && fDist[ny][nx] == 0) {
+					fq.add(new Node(nx, ny));
+					fDist[ny][nx] = fDist[cy][cx] + 1;
+				}
+			}
+		}
+		
+		int answer = -1;
+		while(!jq.isEmpty()) {
+			Node node = jq.poll();
+			int cx = node.x;
+			int cy = node.y;
+			if(cx == 0 || cy == 0 || cx == width-1 || cy == height -1) {
+				answer = jDist[cy][cx];
+				break;
+			}
+			for(int i = 0; i < 4; i++) {
+				int nx = cx + dx[i];
+				int ny = cy + dy[i];
+				if(nx < 0 || ny < 0 || nx >= width || ny >= height) continue;
+				if(matrix[ny][nx] != '#' && jDist[ny][nx] == 0) {
+					int nextDist = jDist[cy][cx] + 1;
+					if(fDist[ny][nx] > nextDist || fDist[ny][nx] == 0) {
+						jq.add(new Node(nx, ny));
+						jDist[ny][nx] = nextDist;
+					}
+				}
+			}
+		}
+			
+		System.out.println(answer == -1 ? "IMPOSSIBLE" : answer);
+	}
+	
+	static class Node {
+		int x, y;
+		Node(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 }
