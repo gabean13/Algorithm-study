c786501e6ff78bdab5f28c09a0f0400c597f57a8
Gabin <choegabin171@gmail.com>
2024-11-16 18:00:32 +0900

    [Gold V] Title: 토마토, Time: 584 ms, Memory: 124176 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,78 +1,71 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-	static public void main(String []args) throws IOException {
-        //1. 입력
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+public class Main {
+	static int width;
+	static int height; 
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		//입력
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		width = Integer.parseInt(st.nextToken());
+		height = Integer.parseInt(st.nextToken());
+		
+		int[][] matrix = new int[height][width];
+		int[][] dist = new int[height][width];
+		Queue<Node> queue = new LinkedList<>();
+		for(int y = 0; y < height; y++) {
+			st = new StringTokenizer(br.readLine());
+			for(int x = 0; x < width; x++) {
+				int num = Integer.parseInt(st.nextToken());
+				if(num == 1) {
+					queue.add(new Node(x, y));
+					dist[y][x] = 1;
+				} 
+				matrix[y][x] = num;
+			}
+		}
+		
+		int[] dx = {1, -1, 0, 0};
+		int[] dy = {0, 0, 1, -1};
+		while(!queue.isEmpty()) {
+			Node node = queue.poll();
+			int cx = node.x;
+			int cy = node.y;
+			for(int i = 0; i < 4; i++) {
+				int nx = cx + dx[i];
+				int ny = cy + dy[i];
+				if(nx < 0 || ny < 0 || nx >= width || ny >= height) continue;
+				if(dist[ny][nx] == 0 && matrix[ny][nx] == 0) {
+					queue.add(new Node(nx, ny));
+					dist[ny][nx] = dist[cy][cx] + 1;
+					matrix[ny][nx] = 1;
+				}
+			}
+		}
 
-        int m = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
-
-        int[] dx = {1, 0, -1, 0};
-        int[] dy  ={0, 1, 0, -1};
-
-        int[][] box = new int[n][m];
-        int[][] dist = new int[n][m];
-
-        Queue<Node> queue = new LinkedList<>();
-        for(int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < m; j++) {
-                box[i][j] = Integer.parseInt(st.nextToken());
-                if(box[i][j] == 1) {
-                    queue.add(new Node(i, j));
-                    dist[i][j] = 0;
-                } else {
-                    dist[i][j] = -1;
-                }
-            } 
-        }
-
-        //2. 계산
-        int days = 0;
-        while(!queue.isEmpty()) {
-            Node node = queue.poll();
-            for(int i = 0; i < 4; i++) {
-                int nx = node.x + dx[i];
-                int ny = node.y + dy[i];
-                if(nx < 0 || nx >= n || ny < 0 || ny >=m) {
-                    continue;
-                }
-                if(box[nx][ny] == 0 && dist[nx][ny] == -1) {
-                    queue.add(new Node(nx, ny));
-                    box[nx][ny] = 1;
-                    dist[nx][ny] = dist[node.x][node.y] + 1;
-                }
-            }
-        }
-
-        boolean isAvailable = true;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(box[i][j] == 0){
-                    isAvailable = false;
-                    break;
-                }
-
-                if(dist[i][j] > days){
-                    days = dist[i][j];
-                }
-            }
-        }
-
-        days = isAvailable ? days : -1;
-        System.out.println(days);
-    }
-}
-
-class Node {
-    int x;
-    int y;
-
-    public Node(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+		int max = 0;
+		for(int y = 0; y < height; y++) {
+			for(int x = 0; x < width; x++) {
+				if(matrix[y][x] == 0) {
+					System.out.println("-1");
+					return;
+				}
+				if(max < dist[y][x]) {
+					max = dist[y][x];
+				}
+			}
+		}
+		
+		System.out.println(max-1);
+	}
+	
+	static class Node {
+		int x, y;
+		Node(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 }
