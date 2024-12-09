e93b63d9fa1760825b5b8ebe3c90fe2ff16ecc1c
Gabin <choegabin171@gmail.com>
2024-12-09 10:11:04 +0900

    [Silver IV] Title: 카드, Time: 344 ms, Memory: 30384 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -1,21 +1,20 @@
 import java.io.*;
-import java.math.BigInteger;
 import java.util.*;
 
 class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Map<BigInteger, Integer> map = new TreeMap<>();
+		Map<Long, Integer> map = new TreeMap<>();
 		
 		int n = Integer.parseInt(br.readLine());
 		for(int i = 0; i < n; i++) {
-			BigInteger key = new BigInteger(br.readLine());
+			Long key = Long.parseLong(br.readLine());
 			map.put(key, map.getOrDefault(key, 0) + 1);
 		}
 		
 		int max = 0;
-		BigInteger answer = new BigInteger("0");
-		for(BigInteger key : map.keySet()) {
+		Long answer = 0L;
+		for(Long key : map.keySet()) {
 			if(map.get(key) > max) {
 				max = map.get(key);
 				answer = key;
