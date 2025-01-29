cd07fb0ba09bd2ce6a5ccc37f85382a527174f1d
Gabin Choi <gazette9@naver.com>
2025-01-29 17:06:19 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.15 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -3,21 +3,11 @@ import java.io.*;
 
 class Solution {
     public long solution(int n) {
-        long answer = 0;
-        //1. 효진이는 1칸 아니면 2칸 뛰기 가능
-        //2. n번째에 도달하면 종료
-        //3. 총 몇가지 방법으로 도달할 수 있는지 세기
-        //4. 근데 그걸 1234567로 나눠야함
         long[] arr = new long[2001];
-        //1 2 3 4 5 6 7 8 9 10
-        //1 2 3 5 
         int cur = 0;
         arr[0] = 1;
         arr[1] = 1;
-        while(true) {
-            if(cur == n) {
-                break;
-            }
+        while(cur < n) {
             if(cur + 2 <= n) {
                 arr[cur+2] = (arr[cur] + arr[cur+1]) % 1234567;
             }
