15e85b5408a41fdac1c1e4d9ea5630dea0069e8f
Gabin Choi <gazette9@naver.com>
2025-10-29 11:04:38 +0900

    [Silver III] Title: 두 수의 합, Time: 256 ms, Memory: 25692 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,30 +1,30 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main {
-	static public void main(String []args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int answer = 0;
-        int[] arr = new int[1000001];
-
-        //입력 처리
-        int n = Integer.parseInt(br.readLine());
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++){
-            arr[Integer.parseInt(st.nextToken())] = 1;
-        }
-
-        int x = Integer.parseInt(br.readLine());
-
-        //구현
-        for(int i = 1; i < (x > 1000001 ? 1000001 : x); i++){
-            int xTargetIdx = x - i;
-            if(arr[i] == 1 && xTargetIdx <= 1000000 && arr[xTargetIdx] == 1){
-                answer++;
-            }
-        }
-
-        System.out.println(answer/2);
-    }
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    
+	    int inputSize = Integer.parseInt(br.readLine());
+	    int[] numbers = new int[inputSize];
+	    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	    for(int i = 0; i < inputSize; i++) {
+	      numbers[i] = Integer.parseInt(st.nextToken());
+	    }
+	    
+	    int answer = 0;
+	    int sum = Integer.parseInt(br.readLine());
+	    boolean[] exists = new boolean[1000001];
+	    for(int current : numbers) {
+	      int complement = sum - current;
+	      if(complement > 0 && complement < 1000001) {
+	        if(exists[complement]) {
+	          answer++;
+	        }
+	        exists[current] = true;
+	      }
+	    }
+	    
+	    System.out.println(answer);
+	}
 }
