0dc40b308eb7ede282b669fa8d884bcd6940f18f
Gabin Choi <gazette9@naver.com>
2025-10-29 12:20:28 +0900

    [Silver II] Title: 에디터, Time: 556 ms, Memory: 129052 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,53 +1,51 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		//초기 문자열 설정
-		char[] init = br.readLine().toCharArray();
-		LinkedList<Character> left = new LinkedList<>();
-		LinkedList<Character> right = new LinkedList<>();
-		for(int i = 0; i < init.length; i++) {
-			left.add(init[i]);
-		}
-		
-		int orderNums = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < orderNums; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			switch(st.nextToken()) {
-			case ("L") : {
-				if(left.size() == 0) break;
-				right.addFirst(left.removeLast());
-				break;
-			}
-			case ("D") : {
-				if(right.size() == 0) break;
-				left.addLast(right.removeFirst());
-				break;
-			}
-			case ("B") : {
-				if(left.size() == 0) break;
-				left.removeLast();
-				break;
-			}
-			case ("P") : {
-				char newStr = st.nextToken().charAt(0);
-				left.addLast(newStr);
-			}
-			}
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		while(!left.isEmpty()) {
-			sb.append(left.removeFirst());
-		}
-		while(!right.isEmpty()) {
-			sb.append(right.removeFirst());
-		}
-		
-		System.out.println(sb);
+	public static void main(String[] args) throws IOException {
+	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	  LinkedList<Character> left = new LinkedList<>();
+	  LinkedList<Character> right = new LinkedList<>();
+	  
+    for(char ch : br.readLine().toCharArray()) {
+      left.add(ch);
+    }
+    
+    int orderCount = Integer.parseInt(br.readLine());
+    for(int i = 0; i < orderCount; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      switch(st.nextToken()) {
+        case("L") : {
+          if(left.isEmpty()) break;
+          right.addFirst(left.removeLast());
+          break;
+        }
+        case("D") : {
+          if(right.isEmpty()) break;
+          left.addLast(right.removeFirst());
+          break;
+        }
+        case("B") : {
+          if(left.isEmpty()) break;
+          left.removeLast();
+          break;
+        }
+        case("P") : {
+          char addStr = st.nextToken().charAt(0);
+          left.addLast(addStr);
+          break;
+        }
+      }
+    }
+    
+    StringBuilder sb = new StringBuilder();
+    for(char str : left) {
+      sb.append(str);
+    }
+    for(char str : right) {
+      sb.append(str);
+    }
+    
+    System.out.println(sb.toString());
 	}
 }
