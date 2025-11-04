4d3bf33d79c644d385c24a6a170d2e38f4f99fe4
Gabin Choi <gazette9@naver.com>
2025-11-04 16:11:49 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,6 @@
+SELECT b.TITLE, b.BOARD_ID, r.REPLY_ID, r.WRITER_ID, r.CONTENTS, to_char(r.CREATED_DATE, 'YYYY-MM-DD') as CREATED_DATE
+from USED_GOODS_BOARD b
+join USED_GOODS_REPLY r
+on b.BOARD_ID = r.BOARD_ID
+where b.CREATED_DATE between to_date('2022-10-01', 'YYYY-MM-DD') and to_date('2022-10-31', 'YYYY-MM-DD')
+order by r.CREATED_DATE, b.TITLE;
