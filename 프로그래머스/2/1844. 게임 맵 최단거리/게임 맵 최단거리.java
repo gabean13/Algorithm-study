274319e700647a55b90c7bed319077ffb0588713
Gabin Choi <gazette9@naver.com>
2025-11-10 17:47:47 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.55 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,58 +1,48 @@
 import java.util.*;
 
-class Node{
-    int x;
-    int y;
-    int cnt;
-    
-    Node (int x, int y, int cnt){
-        this.x = x;
-        this.y = y;
-        this.cnt = cnt;
-    }
-}
-
 class Solution {
     public int solution(int[][] maps) {
-        //1. 최단 경로 + 격자 => BFS
-        int answer = -1;
         Queue<Node> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
         
-        //2. 출발점 할당
-        Node node = new Node(0, 0, 1);
-        queue.add(node);
+        int height = maps.length;
+        int width = maps[0].length;
+        boolean[][] visited = new boolean[height][width];
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+        
+        queue.add(new Node(0, 0, 1));
         visited[0][0] = true;
         
-        //3. 방향별 dx, dy (상, 하, 좌, 우)
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {-1, 1, 0, 0};
-    
-        while(!queue.isEmpty()){
-            Node cn = queue.remove();
-            if((cn.x == maps[0].length-1) && (cn.y == maps.length-1)){
-                answer = cn.cnt;
+        while(!queue.isEmpty()) {
+            Node node = queue.poll();
+            int cx = node.x;
+            int cy = node.y;
+            int cdist = node.dist;
+            if(cx == (width - 1) && cy == (height - 1)) {
+                return cdist;
             }
-            for(int i = 0; i < 4; i++){
-                int nx = cn.x + dx[i];
-                int ny = cn.y + dy[i];
-                
-                //해당 방향에 격자가 존재하고
-                if(0 <= nx && nx < maps[0].length && 0 <= ny && ny < maps.length){
-                    //방문하지 않았고
-                    if(!visited[ny][nx]){
-                        //0이 아니고
-                        if(maps[ny][nx] != 0){
-                            Node newNode = new Node(nx, ny, cn.cnt+1);
-                            queue.add(newNode);
-                            visited[ny][nx] = true;
-                        }
-                    }
-                }
+            for(int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+                if(nx < 0 || ny < 0 || nx >= width || ny >= height) continue;
+                if(visited[ny][nx] || maps[ny][nx] == 0) continue;
+                queue.add(new Node(nx, ny, cdist+1));
+                visited[ny][nx] = true;
             }
         }
         
-        return answer;
+        return -1;
+    }
+    
+    class Node {
+        int x;
+        int y;
+        int dist;
+        
+        public Node(int x, int y, int dist) {
+            this.x = x;
+            this.y = y;
+            this.dist = dist;
+        }
     }
 }
-
