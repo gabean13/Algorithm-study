f38e3be4614aa1b1b54b7f504acd9b2a7370ed28
Gabin Choi <gazette9@naver.com>
2026-01-13 11:18:17 +0900

    [Silver V] Title: 수들의 합 5, Time: 144 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -7,36 +7,26 @@ public class Main {
 	    // N 입력
 	    int N = Integer.parseInt(br.readLine());
 	    
-	    // 1 ~ N까지 배열 선언
-	    int[] arr = new int[N+1];
-	    for(int i = 1; i <= N; i++) {
-	      arr[i] = i;
-	    }
-	    
 	    // answer = 1 선언
 	    int answer = 1;
-	    
-	    // while(arr[right] < N)까지 돈다 
-	    //  l < r 이어야함
-	    int l = 0;
+	    int l = 1;
 	    int r = 1;
-	    long sum = arr[l] + arr[r];
-	    while(arr[r] < N && r < N && l <= r) {
+	    long sum = 1;
+	    while(r < N) {
 	      if(sum > N) {
-	        sum -= arr[l];
+	        sum -= l;
 	        l++;
 	      }
 	      
 	      if(sum < N) {
 	        r++;
-	        sum += arr[r];
+	        sum += r;
 	      }
 	      
 	      if(sum == N) {
 	        answer++;
 	        r++;
-	        sum = sum + arr[r] - arr[l];
-	        l++;
+	        sum = sum + r;
 	      }
 	    }
 	    
