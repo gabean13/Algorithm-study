a6a976145f0fa4a4723b256a4be8f541389c3cff
Gabin Choi <gazette9@naver.com>
2026-01-16 11:52:33 +0900

    [Silver II] Title: DNA 비밀번호, Time: 484 ms, Memory: 86032 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -2,12 +2,15 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+  
+  static int aIdx = 0;
+	static int cIdx = 1;
+	static int gIdx = 2;
+	static int tIdx = 3;
+	static int[] current = new int[4];
+	static int[] wanted = new int[4];
+	
 	public static void main(String[] args) throws IOException {
-	    int aIdx = 0;
-	    int cIdx = 1;
-	    int gIdx = 2;
-	    int tIdx = 3;
-	    
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    StringTokenizer st = new StringTokenizer(br.readLine());
 	    
@@ -15,17 +18,43 @@ public class Main {
 	    int pwLength = Integer.parseInt(st.nextToken());
 	    String[] strArr = br.readLine().split("");
 	    
-	    int[] wanted = new int[4];
+	    
 	    st = new StringTokenizer(br.readLine());
 	    for(int i = 0; i < 4; i++) {
 	      wanted[i] = Integer.parseInt(st.nextToken());
 	    }
 	    
 	    // 초기 값 할당
-	    int[] current = new int[4];
+	    int answer = 0;
 	    for(int i = 0; i < pwLength; i++) {
-	      String str = strArr[i];
-	      if(str.equals("A")) {
+	      add(strArr[i]);
+	    }
+	    if(checkPw()) {
+	      answer++;
+	    }
+	   
+	    for(int i = pwLength; i < N; i++) {
+         add(strArr[i]);
+         remove(strArr[i-pwLength]);
+         if(checkPw()) {
+           answer++;
+         }
+	    }
+	    
+	    System.out.println(answer);
+	}
+	
+	private static boolean checkPw() {
+	  for(int i = 0; i < 4; i++) {
+	    if(wanted[i] > current[i]) {
+	      return false;
+	    }
+	  }
+	  return true;
+	}
+	
+	private static void add(String str) {
+	   if(str.equals("A")) {
 	        current[aIdx]++;
 	      } else if(str.equals("C")) {
 	        current[cIdx]++;
@@ -34,49 +63,17 @@ public class Main {
 	      } else if(str.equals("T")) {
 	        current[tIdx]++;
 	      }
-	    }
-	    
-	    int answer = 0;
-	    for(int i = 0; i <= N - pwLength; i++) {
-	      boolean isPass = true;
-	      for(int j = 0; j < 4; j++) {
-	        if(current[j] < wanted[j]) {
-	          isPass = false;
-	          break;
-	        }
-	      }
-	      if(isPass) {
-	       answer++;
-	      }
-	      
-	      String startStr = strArr[i];
-	      if(i + pwLength >= N) {
-	        break;
-	      }
-	      String newStartStr = strArr[i + pwLength];
-	      if(startStr.equals("A")) {
-	        current[aIdx]--;
-	      } else if(startStr.equals("C")) {
-	        current[cIdx]--;
-	      } else if(startStr.equals("G")) {
-	        current[gIdx]--;
-	      } else if(startStr.equals("T")) {
-	        current[tIdx]--;
-	      }
-	      
-	      if(newStartStr.equals("A")) {
-	        current[aIdx]++;
-	      } else if(newStartStr.equals("C")) {
-	        current[cIdx]++;
-	      } else if(newStartStr.equals("G")) {
-	        current[gIdx]++;
-	      } else if(newStartStr.equals("T")) {
-	        current[tIdx]++;
-	      }
-	    }
-	    
-	    System.out.println(answer);
 	}
 	
-	
+	private static void remove(String str) {
+	   if(str.equals("A")) {
+	        current[aIdx]--;
+	      } else if(str.equals("C")) {
+	        current[cIdx]--;
+	      } else if(str.equals("G")) {
+	        current[gIdx]--;
+	      } else if(str.equals("T")) {
+	        current[tIdx]--;
+	      }
+	}
 }
