1bb0a80d6400ec7e571ac62a15bf7b6bf237ac6a
Gabin Choi <gazette9@naver.com>
2026-01-28 10:27:40 +0900

    [Bronze II] Title: 수 정렬하기, Time: 108 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,70 +1,23 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main {
-	static int[] sorted;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		sorted = new int[n];
-		for(int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		
-		mergeSort(arr, 0, arr.length - 1);
-		
-		StringBuilder sb = new StringBuilder();
-		for(int num : arr) {
-			sb.append(num).append("\n");
-		}
-		
-		System.out.println(sb);
-	}
-	
-	static void mergeSort(int[] arr, int left, int right) {
-		//리스트가 1개인 경우
-		if(left == right) return;
-		
-		int mid = (left + right) / 2;
-		mergeSort(arr, left, mid);
-		mergeSort(arr, mid+1, right);
-		
-		merge(arr, left, mid, right);
-	}
-	
-	/*
-	 * @param arr 정렬할 배열
-	 * @param left 배열의 시작점
-	 * @param mid 배열의 중간점
-	 * @param right 배열의 끝 점
-	 */
-	static void merge(int[] arr, int left, int mid, int right) {
-		int l = left;
-		int r = mid + 1;
-		int idx = left;
-		
-		while(l <= mid && r <= right) {
-			if(arr[l] <= arr[r]) {
-				sorted[idx++] = arr[l++];
-			} else {
-				sorted[idx++] = arr[r++];
-			}
-		}
-		
-		if(l > mid) {
-			while(r <= right) {
-				sorted[idx++] = arr[r++];
-			}
-		} else {
-			while(l <= mid) {
-				sorted[idx++] = arr[l++];
-			}
-		}
-		
-		for(int i = left; i <= right; i++) {
-			arr[i] = sorted[i];
-		}
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int n = Integer.parseInt(br.readLine());
+	    int[] arr = new int[n];
+	    
+	    for(int i = 0; i < n; i++) {
+	      arr[i] = Integer.parseInt(br.readLine());
+	    }
+	    
+	    Arrays.sort(arr);
+	    
+	    StringBuilder sb = new StringBuilder();
+	    for(int i = 0; i < n; i++) {
+	      sb.append(arr[i]).append("\n");
+	    }
+	    
+	    System.out.println(sb.toString());
 	}
 }
