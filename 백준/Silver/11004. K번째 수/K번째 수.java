e6cc1b4465c391877c12421eb9e7ef2297170bc2
Gabin Choi <gazette9@naver.com>
2026-01-30 11:55:59 +0900

    [Silver V] Title: K번째 수, Time: 1880 ms, Memory: 378892 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  static int[] arr;
+  
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	    int n = Integer.parseInt(st.nextToken());
+	    int k = Integer.parseInt(st.nextToken());
+	    arr = new int[n];
+	    st = new StringTokenizer(br.readLine(), " ");
+	    for(int i = 0; i < n; i++) {
+	      arr[i] = Integer.parseInt(st.nextToken());
+	    }
+	    
+	    Arrays.sort(arr);
+	    
+	    System.out.println(arr[k-1]);
+	}
+}
