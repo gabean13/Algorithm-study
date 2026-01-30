aded6367596b4d261bf75656af511bb2be42a0f7
Gabin Choi <gazette9@naver.com>
2026-01-30 11:05:19 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 15384 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,25 +1,39 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
-		int num = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] waiting = new int[num];
-		int[] totalTime = new int[num];
-		for(int i = 0; i < num; i++) {
-			waiting[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(waiting);
-		totalTime[0] = waiting[0];
-		for(int i = 1; i < num; i++) {
-			totalTime[i] = totalTime[i-1] + waiting[i];
-		}
-		
-		int answer = Arrays.stream(totalTime).sum();
-		System.out.println(answer);
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int n = Integer.parseInt(br.readLine());
+	    int[] arr = new int[n];
+	    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	    for(int i = 0; i < n; i++) {
+	      arr[i] = Integer.parseInt(st.nextToken());
+	    }
+	    
+	    for(int i = 1; i < n; i++) {
+	      int insertionValue = arr[i];
+	      int insertionIdx = i;
+	      for(int j = 0; j < i; j++) {
+	        if(insertionValue < arr[j]) {
+	          insertionIdx = j;
+	          break;
+	        }
+	      }
+	      
+	      for(int j = i; j > insertionIdx; j--) {
+	        int temp = arr[j-1];
+	        arr[j-1] = arr[j];
+	        arr[j] = temp;
+	      }
+	      arr[insertionIdx] = insertionValue;
+	    }
+	    
+	    long answer = 0;
+	    for(int i = 0; i < n; i++) {
+	      answer += (arr[i] * (n-i));
+	    }
+	    
+	    System.out.println(answer);
 	}
 }
