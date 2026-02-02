4ae3345560e037c97593b9bab4d21cf33451d5ce
Gabin Choi <gazette9@naver.com>
2026-02-02 11:23:41 +0900

    [Gold III] Title: 나머지 합, Time: 376 ms, Memory: 120764 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -4,33 +4,32 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    // N, M 입력
 	    StringTokenizer st = new StringTokenizer(br.readLine());
-	    int N = Integer.parseInt(st.nextToken());
-	    int M = Integer.parseInt(st.nextToken());
 	    
-	    // long 배열 입력 이때 (합 + 입력 % M) % M으로 나머지의 합 저장
-	    st = new StringTokenizer(br.readLine());
-	    long[] sums = new long[N];
-	    sums[0] = Long.parseLong(st.nextToken()) % M;
-	    for(int i = 1; i < N; i++) {
-	      sums[i] = (Long.parseLong(st.nextToken()) % M + sums[i-1]) % M;
-	    }
+	    int n = Integer.parseInt(st.nextToken());
+	    int m = Integer.parseInt(st.nextToken());
 	    
-	    long[] c = new long[M];
 	    long answer = 0;
-	    for(int i = 0; i < N; i++) {
-	      int remainder = (int) (sums[i] % M);
-	      if(remainder == 0) answer++;
-	      c[remainder]++;
+	    long[] sumArr = new long[n];
+	    st = new StringTokenizer(br.readLine());
+	    sumArr[0] = Long.parseLong(st.nextToken()) % m;
+	    for(int i = 1; i < n; i++) {
+	      long num = Long.parseLong(st.nextToken()) % m;
+	      sumArr[i] = (sumArr[i-1] + num) % m;
 	    }
 	    
-	    for(int i = 0; i < M; i++) {
-	      if(c[i] > 1) {
-	        answer += (c[i] * (c[i] - 1) / 2);
+	    long[] countArr = new long[m];
+	    for(int i = 0; i < n; i++) {
+	      int count = (int) sumArr[i];
+	      countArr[count]++;
+	    }
+	    
+	    for(int i = 0; i < m; i++) {
+	      if(countArr[i] > 1) {
+	        answer += countArr[i] * (countArr[i] - 1) / 2;
 	      }
 	    }
 	    
-	    System.out.println(answer);
+	    System.out.println(answer + countArr[0]);
 	}
 }
