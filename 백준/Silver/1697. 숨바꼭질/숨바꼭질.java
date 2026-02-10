8e90b4db40fa61653d0a1449137d84b31a5ba1f8
Gabin Choi <gazette9@naver.com>
2026-02-10 13:16:26 +0900

    [Silver I] Title: 숨바꼭질, Time: 116 ms, Memory: 18528 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,47 +1,46 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String...args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int subin = Integer.parseInt(st.nextToken());
-		int sister = Integer.parseInt(st.nextToken());
-		
-		int[] subinCnt = new int[100001];
-		Queue<Integer> queue = new LinkedList<>();
-		queue.add(subin);
-		subinCnt[subin] = 1;
-		
-		//0이면 방문 안한거
-		int answer = 0;
-		while(!queue.isEmpty()) {
-			int cx = queue.poll();
-			if(cx == sister) {
-				answer = subinCnt[cx];
-				break;
-			}
-			// +1
-			int nx = cx + 1;
-			if(nx >= 0 && nx <= 100000 && subinCnt[nx] == 0) {
-				queue.add(nx);
-				subinCnt[nx] = subinCnt[cx] + 1;
-			}
-			// -1
-			nx = cx - 1;
-			if(nx >= 0 && nx <= 100000 && subinCnt[nx] == 0) {
-				queue.add(nx);
-				subinCnt[nx] = subinCnt[cx] + 1;
-			}
-			// *2
-			nx = cx*2;
-			if(nx >= 0 && nx <= 100000 && subinCnt[nx] == 0) {
-				queue.add(nx);
-				subinCnt[nx] = subinCnt[cx] + 1;
-			}
-		}
-		
-		System.out.println(answer-1);
-	}
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+	    
+	    int sx = Integer.parseInt(st.nextToken());
+	    int dongx = Integer.parseInt(st.nextToken());
+      
+	    int[] dist = new int[100001];
+	    LinkedList<Integer> q = new LinkedList<>();
+	    q.add(sx);
+	    dist[sx] = 1;
+	    
+	    int answer = 0;
+	    while(!q.isEmpty()) {
+	      int c = q.poll();
+	      if(c == dongx) {
+	        answer = dist[c];
+	        break;
+	      }
+	   
+	      int nx = c + 1;
+	      if(nx >= 0 && nx <= 100000 && dist[nx] == 0) {
+	        q.add(nx);
+	        dist[nx] = dist[c] + 1;
+	      }
+	      
+	      nx = c - 1;
+	      if(nx >= 0 && nx <= 100000 && dist[nx] == 0) {
+	        q.add(nx);
+	        dist[nx] = dist[c] + 1;
+	      }
+	      
+	      nx = c * 2;
+	      if(nx >= 0 && nx <= 100000 && dist[nx] == 0) {
+	        q.add(nx);
+	        dist[nx] = dist[c] + 1;
+	      }
+	    }
+	    
+	    System.out.println(answer - 1);
+  }
 }
