344ca034fc8b148b1c7aac15b588c057915cfef6
Gabin Choi <gazette9@naver.com>
2026-02-12 19:09:49 +0900

    [Silver I] Title: 곱셈, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,24 +1,22 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		long a = Long.parseLong(st.nextToken());
-		long b = Long.parseLong(st.nextToken());
-		long c = Long.parseLong(st.nextToken());
-		
-		System.out.println(recurv(a, b, c));
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+	    
+	    long a = Long.parseLong(st.nextToken());
+	    long b = Long.parseLong(st.nextToken());
+	    long c = Long.parseLong(st.nextToken());
+	    
+	    System.out.println(pow(a, b, c));
 	}
 	
-	static long recurv(long a, long b, long c) {
-		if(b == 1) return a % c;
-		long val = recurv(a, b/2, c);
-		val = val * val % c;
-		if(b%2 == 0) {
-			return val;
-		}
-		return val * a % c;
+	static long pow(long a, long b, long c) {
+	  if(b == 1) return a % c;
+	  long val = pow(a, b/2, c);
+	  if(b % 2 == 0) return val * val % c;
+	  return (val * val % c) * a % c;
 	}
 }
