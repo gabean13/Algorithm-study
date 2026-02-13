57c757434a8eb95be5067199a1556eb8cfce2954
Gabin Choi <gazette9@naver.com>
2026-02-13 17:15:40 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 15736 ms, Memory: 101784 KB -BaekjoonHub

--- 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -1,29 +1,25 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int standard = 1_000_000;
-		//입력
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[2000001];
-		
-		for(int i = 0; i < n; i++) {
-			int num = Integer.parseInt(br.readLine()) + standard;
-			arr[num] += 1;
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < 2000001; i++) {
-			if(arr[i] == 0) continue;
-			int realValue = i - standard;
-			while(arr[i] != 0) {
-				sb.append(realValue).append("\n");
-				arr[i]--;
-			}
-		}
-		
-		System.out.println(sb);
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int[] arr = new int[2000001];
+	    int num = 1000000;
+	    int n = Integer.parseInt(br.readLine());
+	    for(int i = 0; i < n; i++) {
+	      int idx = Integer.parseInt(br.readLine()) + num;
+	      arr[idx]++;
+	    }
+	    
+	    StringBuilder sb = new StringBuilder();
+	    for(int i = 0; i < 2000001; i++) {
+	      while(arr[i] > 0) {
+	        arr[i]--;
+	        sb.append(i - num).append("\n");
+	      }
+	    }
+	    
+	    System.out.println(sb.toString());
 	}
 }
