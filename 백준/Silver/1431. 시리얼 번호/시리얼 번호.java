99ea5571ad61eb823a813cf618c5951b4453c3a1
Gabin Choi <gazette9@naver.com>
2026-02-13 18:22:42 +0900

    [Silver III] Title: 시리얼 번호, Time: 96 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/1431. 시리얼 번호/시리얼 번호.java
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.java
@@ -1,58 +1,42 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//1. 길이비교
-		//2. 길이가 같다면 숫자의 합 비교
-		//3. 숫자의 합까지 같다면 사전순 비교
-		
-		int n = Integer.parseInt(br.readLine());
-		List<String> list = new ArrayList<>();
-		for(int i = 0; i < n; i++) {
-			String serial = br.readLine();
-			list.add(serial);
-		}
-		
-		Collections.sort(list, new Comparator<String>() {
-			@Override
-			public int compare(String s1, String s2) {
-				if(s1.length() != s2.length()) {
-					return s1.length() - s2.length();
-				} else {
-					int s1Num = numberSum(s1);
-					int s2Num = numberSum(s2);
-					if(s1Num != s2Num) {
-						return s1Num - s2Num;
-					} else {
-						for(int i = 0; i < s1.length(); i++) {
-							if(s1.charAt(i) == s2.charAt(i)) continue;
-							return s1.compareTo(s2);
-						}
-						return 0;
-					}
-				}
-			}
-		});
-		
-		StringBuilder sb = new StringBuilder();
-		for(String str : list) {
-			sb.append(str).append("\n");
-		}
-		
-		System.out.println(sb);
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    
+	    int n = Integer.parseInt(br.readLine());
+	    List<Guitar> list = new ArrayList<>();
+	    for(int i = 0; i < n; i++) {
+	      list.add(new Guitar(br.readLine()));
+	    }
+	    
+	    Collections.sort(list);
+	    for(Guitar g : list) {
+	      System.out.println(g.serial);
+	    }
 	}
-	
-	static int numberSum(String str) {
-		int total = 0;
-		char[] arr = str.toCharArray();
-		for(char s : arr) {
-			if(s >= '0' && s <= '9') {
-				total += (int)(s - '0');
-			}
-		}
-		
-		return total;
-	}
+}
+
+class Guitar implements Comparable<Guitar>{
+  String serial;
+  int countSum;
+  
+  public Guitar(String serial) {
+    this.serial = serial;
+    this.countSum = 0;
+    for(char str: serial.toCharArray()) {
+      if(str >= '0' && str <= '9') {
+        countSum += (int)(str - '0');
+      }
+    }
+  }
+  
+  @Override
+  public int compareTo(Guitar o1) {
+    String o1s = o1.serial;
+    if(serial.length() != o1s.length()) return serial.length() - o1s.length();
+    if(countSum != o1.countSum) return countSum - o1.countSum;
+    return serial.compareTo(o1s);
+  }
 }
