c7e2b991a98c73d5d60245e2a80a8a26c465f37e
Gabin Choi <gazette9@naver.com>
2026-03-31 09:57:30 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18056 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,23 +1,23 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int x = Integer.parseInt(br.readLine());
-		
-		int[] arr = new int[x+1];
-		arr[1] = 0;
-		for(int i = 2; i <= x; i++) {
-			arr[i] = arr[i-1] + 1;
-			if(i % 2 == 0) {
-				arr[i] = Math.min(arr[i], arr[i/2] + 1);
-			}
-			if(i % 3 == 0) {
-				arr[i] = Math.min(arr[i], arr[i/3] + 1);
-			} 
-		}
-		
-		System.out.println(arr[x]);
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int num = Integer.parseInt(br.readLine());
+      
+      int[] dist = new int[num+1];
+      for(int i = 2; i <= num; i++) {
+        int min = dist[i-1];
+        if(i%3 == 0) {
+          min = Math.min(min, dist[i/3]);
+        }
+        if(i%2 == 0) {
+          min = Math.min(min, dist[i/2]);
+        }
+        dist[i] = min + 1;
+      }
+      
+      System.out.println(dist[num]);
 	}
 }
